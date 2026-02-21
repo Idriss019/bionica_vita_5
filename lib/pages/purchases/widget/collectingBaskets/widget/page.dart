8ca650e7ca7e0bf56747395e00d6d315dd9071e2
Idriss019 <148658965+Idriss019@users.noreply.github.com>
2026-02-22 01:40:59 +0300

    update 0222

--- lib/pages/purchases/widget/collectingBaskets/widget/page.dart
+++ lib/pages/purchases/widget/collectingBaskets/widget/page.dart
@@ -31,116 +31,116 @@ class CollectingBasketsPage extends StatelessWidget {
               colorLineBorder: colorLineBorder,
             ),
             /* */
-            Container(
-              height: gridHeight,
-              decoration: BoxDecoration(
-                border: Border.all(color: colorLineBorder, width: 0.5),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Row(
-                      children: [
-                        Container(
-                          width: 150,
-                          margin: EdgeInsets.only(left: leftRightMargin),
-                          child: Text(
-                            'Доставка транс. компании',
-                            style: textStyle.copyWith(fontSize: 16),
-                          ),
-                        ),
-                        Container(
-                          width: 160,
-                          margin: EdgeInsets.only(
-                            left: leftRightMargin,
-                            // right: leftRightMargin,
-                          ),
-                          height: borderHeight,
-                          child: TextFieldCustom(textStyle: textStyle),
-                        ),
-                        Expanded(
-                          child: Container(
-                            width: 100,
-                            height: gridHeight,
-                            margin: EdgeInsets.only(
-                              left: leftRightMargin,
-                              // right: leftRightMargin,
-                              top: 10,
-                              bottom: 10,
-                            ),
-                            child: DropdownMenu<String>(
-                              initialSelection: 'Вариант 1',
-                              label: const Text('Величина'),
-                              dropdownMenuEntries: <DropdownMenuEntry<String>>[
-                                DropdownMenuEntry(value: '1', label: 'КГ'),
-                                DropdownMenuEntry(value: '2', label: 'СМ'),
-                                DropdownMenuEntry(value: '3', label: 'Цена'),
-                              ],
-                              onSelected: (String? value) {
-                                print('Выбрано: $value');
-                              },
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+            // Container(
+            //   height: gridHeight,
+            //   decoration: BoxDecoration(
+            //     border: Border.all(color: colorLineBorder, width: 0.5),
+            //     borderRadius: BorderRadius.all(Radius.circular(10)),
+            //   ),
+            //   child: Row(
+            //     children: [
+            //       Expanded(
+            //         child: Row(
+            //           children: [
+            //             Container(
+            //               width: 150,
+            //               margin: EdgeInsets.only(left: leftRightMargin),
+            //               child: Text(
+            //                 'Доставка транс. компании',
+            //                 style: textStyle.copyWith(fontSize: 16),
+            //               ),
+            //             ),
+            //             Container(
+            //               width: 160,
+            //               margin: EdgeInsets.only(
+            //                 left: leftRightMargin,
+            //                 // right: leftRightMargin,
+            //               ),
+            //               height: borderHeight,
+            //               child: TextFieldCustom(textStyle: textStyle),
+            //             ),
+            //             Expanded(
+            //               child: Container(
+            //                 width: 100,
+            //                 height: gridHeight,
+            //                 margin: EdgeInsets.only(
+            //                   left: leftRightMargin,
+            //                   // right: leftRightMargin,
+            //                   top: 10,
+            //                   bottom: 10,
+            //                 ),
+            //                 child: DropdownMenu<String>(
+            //                   initialSelection: 'Вариант 1',
+            //                   label: const Text('Величина'),
+            //                   dropdownMenuEntries: <DropdownMenuEntry<String>>[
+            //                     DropdownMenuEntry(value: '1', label: 'КГ'),
+            //                     DropdownMenuEntry(value: '2', label: 'СМ'),
+            //                     DropdownMenuEntry(value: '3', label: 'Цена'),
+            //                   ],
+            //                   onSelected: (String? value) {
+            //                     print('Выбрано: $value');
+            //                   },
+            //                 ),
+            //               ),
+            //             ),
+            //           ],
+            //         ),
+            //       ),
 
-                  Expanded(
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Container(
-                            margin: EdgeInsets.only(left: leftRightMargin),
-                            child: Text(
-                              'Доп. расход на дорогу',
-                              style: textStyle.copyWith(fontSize: 16),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            margin: EdgeInsets.only(
-                              // left: leftRightMargin,
-                              right: leftRightMargin,
-                            ),
-                            height: borderHeight,
-                            child: TextFieldCustom(textStyle: textStyle),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            /* Find */
-            Container(
-              height: gridHeight,
-              decoration: BoxDecoration(
-                border: Border.all(color: colorLineBorder, width: 0.5),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(left: leftRightMargin),
-                    child: Text('Поиск корзины', style: textStyle),
-                  ),
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.only(
-                        left: leftRightMargin,
-                        right: leftRightMargin,
-                      ),
-                      height: borderHeight,
-                      child: TextFieldCustom(textStyle: textStyle),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            //       Expanded(
+            //         child: Row(
+            //           children: [
+            //             Expanded(
+            //               child: Container(
+            //                 margin: EdgeInsets.only(left: leftRightMargin),
+            //                 child: Text(
+            //                   'Доп. расход на дорогу',
+            //                   style: textStyle.copyWith(fontSize: 16),
+            //                 ),
+            //               ),
+            //             ),
+            //             Expanded(
+            //               child: Container(
+            //                 margin: EdgeInsets.only(
+            //                   // left: leftRightMargin,
+            //                   right: leftRightMargin,
+            //                 ),
+            //                 height: borderHeight,
+            //                 child: TextFieldCustom(textStyle: textStyle),
+            //               ),
+            //             ),
+            //           ],
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+            // /* Find */
+            // Container(
+            //   height: gridHeight,
+            //   decoration: BoxDecoration(
+            //     border: Border.all(color: colorLineBorder, width: 0.5),
+            //     borderRadius: BorderRadius.all(Radius.circular(10)),
+            //   ),
+            //   child: Row(
+            //     children: [
+            //       Container(
+            //         margin: EdgeInsets.only(left: leftRightMargin),
+            //         child: Text('Поиск корзины', style: textStyle),
+            //       ),
+            //       Expanded(
+            //         child: Container(
+            //           margin: EdgeInsets.only(
+            //             left: leftRightMargin,
+            //             right: leftRightMargin,
+            //           ),
+            //           height: borderHeight,
+            //           child: TextFieldCustom(textStyle: textStyle),
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
             /* TableBaskets */
             Container(
               height: gridHeight * 3,
